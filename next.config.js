35a0c6499c546aeae9a6ddf429ce3f02b5d11f49
linsonlorance <145975098+linsonlorance@users.noreply.github.com>
2023-11-21 17:04:57 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,13 +2,7 @@
 const path = require("path");
 
 const nextConfig = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
-  },
-  webpack: (config) => {
-    config.resolve.alias["@app"] = path.resolve(__dirname, "app");
-    return config;
-  },
+
 };
 
 module.exports = nextConfig;
